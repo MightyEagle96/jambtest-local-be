f2ce3655f7832cd254980594b9653b0f2da4485a
MIGHTY EAGLE <mightyeaglecorp@gmail.com>
2025-11-18 08:33:56 +0100

    made changes

--- dist/database.js
+++ dist/database.js
@@ -1,27 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConnectDatabase = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/jambtest";
-console.log(mongoUri);
-const ConnectDatabase = () => {
-    mongoose_1.default
-        .connect(mongoUri, {
-        connectTimeoutMS: 60000,
-        serverSelectionTimeoutMS: 60000,
-    })
-        .then(() => {
-        console.log("Database connected successfully");
-    })
-        .catch((e) => {
-        console.log(e);
-        console.log("DB could not connect at this time. Shutting down");
-        process.exit(1);
-    });
-};
 exports.ConnectDatabase = ConnectDatabase;
+const mongoose_1 = __importDefault(require("mongoose"));
+const mongodb_memory_server_1 = require("mongodb-memory-server");
+// dotenv.config();
+// const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/jambtest";
+// export const ConnectDatabase = () => {
+//   mongoose
+//     .connect("mongodb://localhost:27017/jambtest", {
+//       connectTimeoutMS: 60000,
+//       serverSelectionTimeoutMS: 60000,
+//     })
+//     .then(() => {
+//       console.log("Database connected successfully");
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       console.log("DB could not connect at this time. Shutting down");
+//       process.exit(1);
+//     });
+// };
+function ConnectDatabase() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const mongoServer = yield mongodb_memory_server_1.MongoMemoryServer.create({
+            instance: {
+                ip: "127.0.0.1",
+                port: 0, // random port
+            },
+        });
+        const uri = mongoServer.getUri();
+        yield mongoose_1.default.connect(uri);
+        console.log("Embedded MongoDB started");
+    });
+}
--- src/database.ts
+++ src/database.ts
@@ -1,24 +1,39 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-dotenv.config();
+// dotenv.config();
 
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/jambtest";
+// const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/jambtest";
 
-console.log(mongoUri);
+// export const ConnectDatabase = () => {
+//   mongoose
+//     .connect("mongodb://localhost:27017/jambtest", {
+//       connectTimeoutMS: 60000,
+//       serverSelectionTimeoutMS: 60000,
+//     })
+//     .then(() => {
+//       console.log("Database connected successfully");
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       console.log("DB could not connect at this time. Shutting down");
+//       process.exit(1);
+//     });
+// };
 
-export const ConnectDatabase = () => {
-  mongoose
-    .connect(mongoUri, {
-      connectTimeoutMS: 60000,
-      serverSelectionTimeoutMS: 60000,
-    })
-    .then(() => {
-      console.log("Database connected successfully");
-    })
-    .catch((e) => {
-      console.log(e);
-      console.log("DB could not connect at this time. Shutting down");
-      process.exit(1);
-    });
-};
+async function ConnectDatabase() {
+  const mongoServer = await MongoMemoryServer.create({
+    instance: {
+      ip: "127.0.0.1",
+      port: 0, // random port
+    },
+  });
+
+  const uri = mongoServer.getUri();
+
+  await mongoose.connect(uri);
+  console.log("Embedded MongoDB started");
+}
+
+export { ConnectDatabase };
