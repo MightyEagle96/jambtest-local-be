cdd685cb4dee5189b655a1ba7f355e51c885696f
MIGHTY EAGLE <mightyeaglecorp@gmail.com>
2025-11-19 13:36:34 +0100

    made changes

--- compress.sh
+++ compress.sh
@@ -5,9 +5,12 @@ ZIP_NAME="JAMBTEST.zip"
 echo ">>> Cleaning old zip if it exists..."
 rm -f "$ZIP_NAME"
 
-echo ">>> Compressing bundle.js and package.json into $ZIP_NAME..."
+echo ">>> Compressing build folder, bundle.js, and package.json into $ZIP_NAME..."
 
-zip -j "$ZIP_NAME" dist/bundle.js package.json
+zip -r "$ZIP_NAME" \
+  dist/build \
+  bundle.js \
+  package.json
 
 echo ">>> Done!"
 echo "Created: $ZIP_NAME"
